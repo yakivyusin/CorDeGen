ee0b422e74b38297c4f67a4b3d1190d13b3bf4f4
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-06-29 18:06:49 +0300

    Added Term Distribution Calculation to Dynamic Plugin

--- CorDeGen.Library/DynamicPlugin.cs
+++ CorDeGen.Library/DynamicPlugin.cs
@@ -6,19 +6,25 @@ namespace CorDeGen.Library
     public class DynamicPlugin
     {
         private delegate int TextCountCalculation(int termCount);
+        private delegate int[] TermDistributionCalculation(int termIndex);
 
         private TextCountCalculation _textCountCalculation;
+        private TermDistributionCalculation _termDistributionCalculation;
 
         public DynamicPlugin(object obj) => SetupDelegates(obj);
 
         public int GetTextCount(int termCount) => _textCountCalculation(termCount);
 
+        public int[] GetTermDistribution(int termIndex) => _termDistributionCalculation(termIndex);
+
         private void SetupDelegates(object obj)
         {
             var type = obj.GetType();
             var textCountMethod = type.GetMethod("GetTextCount", BindingFlags.Public | BindingFlags.Instance);
+            var termDistributionMethod = type.GetMethod("GetTermDistribution", BindingFlags.Public | BindingFlags.Instance);
 
             _textCountCalculation = (TextCountCalculation)Delegate.CreateDelegate(typeof(TextCountCalculation), obj, textCountMethod);
+            _termDistributionCalculation = (TermDistributionCalculation)Delegate.CreateDelegate(typeof(TermDistributionCalculation), obj, termDistributionMethod);
         }
     }
 }
