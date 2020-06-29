054b389b266dbb376a65ccbcf296d5ac006216ba
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-06-29 15:35:38 +0300

    Added Dynamic Plugin Loader (Roslyn-based)

--- CorDeGen.Library/DynamicPlugin.cs
+++ CorDeGen.Library/DynamicPlugin.cs
@@ -18,7 +18,7 @@ namespace CorDeGen.Library
             var type = obj.GetType();
             var textCountMethod = type.GetMethod("GetTextCount", BindingFlags.Public | BindingFlags.Instance);
 
-            _textCountCalculation = (TextCountCalculation)Delegate.CreateDelegate(type, obj, textCountMethod);
+            _textCountCalculation = (TextCountCalculation)Delegate.CreateDelegate(typeof(TextCountCalculation), obj, textCountMethod);
         }
     }
 }
---
+++ CorDeGen.Library/DynamicPluginLoader.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.IO;
+using System.Runtime.Loader;
+
+namespace CorDeGen.Library
+{
+    public static class DynamicPluginLoader
+    {
+        public static DynamicPlugin Load(FileInfo fileInfo)
+        {
+            var sourceCode = File.ReadAllText(fileInfo.FullName);
+
+            using (var pluginStream = new MemoryStream())
+            {
+                var result = GenerateCode(sourceCode).Emit(pluginStream);
+
+                if (!result.Success)
+                {
+                    // todo: provide exception
+                }
+
+                pluginStream.Seek(0, SeekOrigin.Begin);
+
+                var assemblyLoadContext = new AssemblyLoadContext("dynamic plugin");
+                var assembly = assemblyLoadContext.LoadFromStream(pluginStream);
+                var pluginType = assembly.GetExportedTypes()[0];
+
+                return new DynamicPlugin(Activator.CreateInstance(pluginType));
+            }
+        }
+
+        private static CSharpCompilation GenerateCode(string sourceCode)
+        {
+            var codeString = SourceText.From(sourceCode);
+            var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.Latest);
+
+            var parsedSyntaxTree = SyntaxFactory.ParseSyntaxTree(codeString, options);
+
+            var references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Console).Assembly.Location)
+            };
+
+            return CSharpCompilation.Create("DynamicPlugin.dll",
+                new[] { parsedSyntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
+                    optimizationLevel: OptimizationLevel.Release,
+                    assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default));
+        }
+    }
+}
