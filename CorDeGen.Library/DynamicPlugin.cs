c21585258e119bc26e0571ddb87ed48da85bd771
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-06-29 15:11:39 +0300

    Added Core Library with Dynamic Plugin Description

---
+++ CorDeGen.Library/DynamicPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace CorDeGen.Library
+{
+    public class DynamicPlugin
+    {
+        private delegate int TextCountCalculation(int termCount);
+
+        private TextCountCalculation _textCountCalculation;
+
+        public DynamicPlugin(object obj) => SetupDelegates(obj);
+
+        public int GetTextCount(int termCount) => _textCountCalculation(termCount);
+
+        private void SetupDelegates(object obj)
+        {
+            var type = obj.GetType();
+            var textCountMethod = type.GetMethod("GetTextCount", BindingFlags.Public | BindingFlags.Instance);
+
+            _textCountCalculation = (TextCountCalculation)Delegate.CreateDelegate(type, obj, textCountMethod);
+        }
+    }
+}
