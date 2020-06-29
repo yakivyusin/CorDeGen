05888f211753baa317c29265fcb1fd56554c6a47
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-06-29 15:48:56 +0300

    Added Corpus Generator Stub

---
+++ CorDeGen.Library/CorpusGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace CorDeGen.Library
+{
+    public class CorpusGenerator
+    {
+        private readonly int _termCount;
+        private readonly int _textCount;
+
+        public CorpusGenerator(int termCount, DynamicPlugin dynamicPlugin)
+        {
+            _termCount = termCount;
+            _textCount = dynamicPlugin.GetTextCount(termCount);
+        }
+
+        public string[] GetCorpus()
+        {
+            var texts = Enumerable
+                .Range(0, _textCount)
+                .Select(x => new StringBuilder())
+                .ToArray();
+
+            return texts
+                .Select(x => x.ToString())
+                .ToArray();
+        }
+    }
+}
