e7ad03b6f7911e594df4d90c95ed956cae026156
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-06-30 19:22:04 +0300

    Added Term Distribution Calculation to Default Plugin

--- CorDeGen/Default.cs
+++ CorDeGen/Default.cs
@@ -1,6 +1,43 @@
 ﻿using System;
+using System.Linq;
 
 public class DefaultPlugin
 {
-    public int GetTextCount(int termCount) => (int)Math.Pow(termCount, 0.25);
+    private int _textCount;
+
+    public int GetTextCount(int termCount)
+    {
+        _textCount = (int)Math.Pow(termCount, 0.25);
+        return _textCount;
+    }
+
+    public int[] GetTermDistribution(int termIndex)
+    {
+        var arr = new int[_textCount];
+        var range = (_textCount / 5) + 1;
+        var centralText = termIndex % _textCount;
+        var occurenceIndexes = Enumerable.Range(centralText - range, 2 * range + 1).Select(x => GetSafeIndex(x, arr));
+
+        foreach (var occurenceIndex in occurenceIndexes)
+        {
+            arr[occurenceIndex] = occurenceIndex == centralText ? 2 : 1;
+        }
+
+        return arr;
+    }
+
+    private int GetSafeIndex(int index, Array array)
+    {
+        if (index < 0)
+        {
+            return array.Length + index;
+        }
+
+        if (index >= array.Length)
+        {
+            return index % array.Length;
+        }
+
+        return index;
+    }
 }
