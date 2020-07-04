2b165384dcf73bc175c778522eb6d53a9f135c90
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-07-04 18:51:49 +0300

    Added Benchmark of Corpus Generation

---
+++ CorDeGen.Benchmark/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using CorDeGen.Library;
+
+namespace CorDeGen.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+
+    public class Benchmark
+    {
+        [Params(100, 1_000, 10_000, 100_000)]
+        public int N { get; set; }
+
+        [Benchmark]
+        public void Generate()
+        {
+            var generator = new CorpusGenerator(N, new DynamicPlugin(new DefaultPlugin()));
+            generator.GetCorpus();
+        }
+    }
+}
