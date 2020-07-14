fb56343cceb484552b0a565e7c4171a62d99a6b9
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-07-14 19:17:44 +0300

    Added Saving of Results to KMeans Subprogram

--- CorDeGen.KMeans/Model/TextData.cs
+++ CorDeGen.KMeans/Model/TextData.cs
@@ -2,6 +2,7 @@
 {
     public class TextData
     {
+        public string FileName { get; set; }
         public string Text { get; set; }
     }
 }
--- CorDeGen.KMeans/Program.cs
+++ CorDeGen.KMeans/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CorDeGen.KMeans.Model;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -12,6 +13,7 @@ namespace CorDeGen.KMeans
                 .OrderBy(file => int.Parse(Path.GetFileNameWithoutExtension(file.Name)))
                 .Select(file => new TextData
                 {
+                    FileName = file.Name,
                     Text = File.ReadAllText(file.FullName)
                 })
                 .ToArray();
@@ -21,6 +23,14 @@ namespace CorDeGen.KMeans
 
             var featurizedData = featurizer.Featurize(texts);
             var clusteredData = clusterer.Clusterize(featurizedData, 2);
+
+            textsDirectory.CreateSubdirectory("result");
+
+            using var resultFile = File.CreateText(Path.Combine(textsDirectory.FullName, $"result\\{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}.txt"));
+            foreach (var text in clusteredData)
+            {
+                resultFile.WriteLine($"{text.FileName} | {text.PredictedClusterId}");
+            }
         }
     }
 }
