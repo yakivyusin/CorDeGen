523d40caedb5eb90408fb52d2196def8f81b5365
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-07-14 19:02:33 +0300

    Added Entry Point with Clustering for KMean SubProgram

--- CorDeGen.KMeans/Program.cs
+++ CorDeGen.KMeans/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace CorDeGen.KMeans
+﻿using CorDeGen.KMeans.Model;
+using System.IO;
+using System.Linq;
+
+namespace CorDeGen.KMeans
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(DirectoryInfo textsDirectory)
         {
-            
+            var texts = textsDirectory.GetFiles("*.txt")
+                .OrderBy(file => int.Parse(Path.GetFileNameWithoutExtension(file.Name)))
+                .Select(file => new TextData
+                {
+                    Text = File.ReadAllText(file.FullName)
+                })
+                .ToArray();
+
+            var featurizer = new TextFeaturizer();
+            var clusterer = new KMeansClusterer();
+
+            var featurizedData = featurizer.Featurize(texts);
+            var clusteredData = clusterer.Clusterize(featurizedData, 2);
         }
     }
 }
